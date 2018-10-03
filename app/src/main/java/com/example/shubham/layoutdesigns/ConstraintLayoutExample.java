86605cfea782e4204e68b953c9465a39158a058b
modishubham <shubham.modi@tothenew.com>
2018-10-03 13:02:08 +0530

    commit for assignment 2

---
+++ app/src/main/java/com/example/shubham/layoutdesigns/ConstraintLayoutExample.java
@@ -0,0 +1,13 @@
+package com.example.shubham.layoutdesigns;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintLayoutExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout_example);
+    }
+}
--- app/src/main/java/com/example/shubham/layoutdesigns/MainActivity.java
+++ app/src/main/java/com/example/shubham/layoutdesigns/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button1,button2;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mlayoutManager;
@@ -24,6 +25,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recyclerview_example);
+
+        button1 = findViewById(R.id.btn1);
+        button2 = findViewById(R.id.btn2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RelativeLayoutExample.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ConstraintLayoutExample.class);
+                startActivity(intent);
+            }
+        });
+
 
         recyclerView = findViewById(R.id.recyclerView);
         mlayoutManager = new LinearLayoutManager(this);
@@ -49,40 +70,40 @@ public class MainActivity extends AppCompatActivity {
         Model model8 = new Model();
         Model model9 = new Model();
 
-        model1.setDesciption("description1");
-        model1.setTitle("Title1");
+        model1.setDesciption("");
+        model1.setTitle("China Says Navy Drove US Warship From South China Sea Reef");
         news.add(model1);
 
-        model2.setDesciption("description2");
-        model2.setTitle("Title2");
+        model2.setDesciption("Ag industry offers praise for new North American free trade pact, which tackles Canada’s dairy program and removes discriminatory grain grading standards");
+        model2.setTitle("Goodbye NAFTA, hello USMCA");
         news.add(model2);
 
-        model3.setDesciption("description3");
-        model3.setTitle("Title3");
+        model3.setDesciption("In a news conference, Trump said he is open to a “comprehensive investigation” but insisted that it must move quickly.");
+        model3.setTitle("White House agrees to expand Kavanaugh probe slightly as McConnell signals vote is imminent");
         news.add(model3);
 
-        model4.setDesciption("description4");
-        model4.setTitle("Title4");
+        model4.setDesciption("As a 19-year-old freshman at Columbia, O’Rourke used the phrase “phenomenally large breasts and tight buttocks“ in reviewing a Broadway musical for the college paper.");
+        model4.setTitle("‘I am ashamed’: Beto apologizes for ‘demeaning comments about women’");
         news.add(model4);
 
-        model5.setDesciption("description5");
-        model5.setTitle("Title5");
+        model5.setDesciption("The blaze wound up causing $8 million in damage.");
+        model5.setTitle("A Border Patrol Agent Started a 47,000-Acre Wildfire with a Gender Reveal Party - VICE");
         news.add(model5);
 
-        model6.setDesciption("description6");
-        model6.setTitle("Title6");
+        model6.setDesciption("A father and son were killed Saturday night when a car exploded in Allentown, the Lehigh Coroner's Office announced Monday afternoon.");
+        model6.setTitle("Father and Toddler Among 3 People Killed in Allentown Car Blast");
         news.add(model6);
 
-        model7.setDesciption("description7");
-        model7.setTitle("Title7");
+        model7.setDesciption("Flash-flood watches stretch a whopping 750 miles, extending from the Mexican border northward through Utah.");
+        model7.setTitle("Tropical Storm Rosa to dump torrential rains on Desert Southwest, leading to flooding Tuesday");
         news.add(model7);
 
-        model8.setDesciption("description8");
-        model8.setTitle("Title8");
+        model8.setDesciption("The president's media critiques got especially personal during a Rose Garden press conference.");
+        model8.setTitle("Trump to reporter: 'I know you’re not thinking, you never do'");
         news.add(model8);
 
-        model9.setDesciption("description9");
-        model9.setTitle("Title9");
+        model9.setDesciption("Survivors described the panic and devastation of the earthquake and tsunami that hit Sulawesi, Indonesia, as rescue workers raced to find survivors and pull them from the rubble.");
+        model9.setTitle("‘We Just Ran’: Scale of Tsunami Horror Emerges in Indonesia");
         news.add(model9);
 
         return news;
